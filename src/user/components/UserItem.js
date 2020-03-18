4c35755f5c1f03923bd3c12356f107c1417a1bba
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-03-18 22:11:05 +0100

    Added users files with dummy data

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './UserItem.css';
+
+const UserItem = (props) => {
+  return <h1>{props.id}</h1>;
+};
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h1>Users</h1>;
+  const users = [
+    {
+      id:'u1',
+      name:"Test user1",
+      image:'www url picture1 jpg',
+      places:3
+    },
+    {
+      id:'u2',
+      name:"Test user2",
+      image:'www url picture2 jpg',
+      places:4
+    }
+  ];
+
+  return <UsersList items={users} />;
 };
 
 export default Users;
