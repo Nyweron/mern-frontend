dc649fab3a2c4b9979097dedec61074ca7d87575
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-03-18 21:51:48 +0100

    Added react router path and small routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
   return (
-    <div className="App">
-TEST
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/places/now" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewPlace = () => {
+  return <h1>NewPlace</h1>;
+};
+
+export default NewPlace;
