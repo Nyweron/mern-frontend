4bd9faef763049200553edf0444a63488dd9b3c5
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-03-19 19:24:33 +0100

    Added navigation for mobile

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -2,24 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import "./MainNavigation.css";
-import NavLinks from './NavLinks';
+import NavLinks from "./NavLinks";
 import MainHeader from "./MainHeader";
+import SideDrawer from "./SideDrawer";
 
 const MainNavigation = props => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlaces</Link>
-      </h1>
-      <nav>
-     <NavLinks />
+    <>
+      <SideDrawer>
+        <nav className="main-navigation__Drawer-nav">
+          <NavLinks />
         </nav>
-    </MainHeader>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 };
 
