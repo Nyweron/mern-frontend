89e044ad7b8ad8411b9f4341c7b390a9c9855601
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-03-19 16:17:39 +0100

    Implemented UsersItem

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-import './UserItem.css';
+import Avatar from '../../shared/components/UIElements/Avatar';
+import "./UserItem.css";
 
-const UserItem = (props) => {
-  return <h1>{props.id}</h1>;
+const UserItem = props => {
+  return (
+    <li className="user-item">
+
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <Avatar image={props.image} alt={props.name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{props.name}</h2>
+          <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default UserItem;
