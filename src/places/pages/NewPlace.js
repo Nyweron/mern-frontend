387d02f3b93142e7c9473724d8509de99e32f3db
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-03-24 18:07:39 +0100

    Implemented input component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-import Input from '../../shared/components/FormElements/Input';
-import './NewPlace.css';
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
 
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter a valid title." />
     </form>
   );
 };
