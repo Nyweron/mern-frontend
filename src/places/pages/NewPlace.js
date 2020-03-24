0c2d0bba609d6ba7f0207a78d933945706e8ffab
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-03-24 21:58:26 +0100

    Added useCallback

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from "../../shared/util/validators";
 import "./NewPlace.css";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {
+
+  }, []);
+
+  const descriptionInputHandler = useCallback((id, value, isValid) => {
+
+  }, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
-        validators={[]}
-        errorText="Please enter a valid title." />
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="description"
+        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description (min length 5 characters)."
+        onInput={descriptionInputHandler}
+      />
     </form>
   );
 };
