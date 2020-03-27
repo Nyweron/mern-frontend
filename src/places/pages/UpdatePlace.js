887635fe6e08a3ce053d16b32755d7eecaa822cb
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-03-27 21:49:39 +0100

    Added updatePlace file

---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import { dummy_places } from "./dummyPlaces";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from "../../shared/util/validators";
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+
+  const identifiedPlace = dummy_places.find(p => p.id === placeId);
+
+  if (!identifiedPlace) {
+    return <div className="center">Could not find place</div>;
+  }
+
+  return (
+    <form>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validator={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title."
+        onInput={() => {}}
+        value={identifiedPlace.title}
+        valid={true}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validator={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid descriptions (min. 5 characters)."
+        onInput={() => {}}
+        value={identifiedPlace.title}
+        valid={true}
+      />
+      <Button type="submit" disabled={true}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -3,33 +3,9 @@ import { useParams } from 'react-router-dom';
 
 
 import PlaceList from '../components/PlaceList';
+import { dummy_places } from "./dummyPlaces";
 
-const dummy_places = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: "One of the most famouse sky scraper in the world",
-    imageUrl:'https://upload.wikimedia.org/wikipedia/commons/c/c7/Empire_State_Building_from_the_Top_of_the_Rock.jpg',
-    address:'20 W 34th St, New York, NY 10001',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584
-    },
-    creator:'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Stonehenge',
-    description: "Stonehenge is a prehistoric World Heritage Site eight miles (13 kilometers) north of Salisbury in Wiltshire, England. It is made of a henge, with standing stones in circles.",
-    imageUrl:'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Stonehenge2007_07_30.jpg/1024px-Stonehenge2007_07_30.jpg',
-    address:'Salisbury SP4 7DE, Wielka Brytania',
-    location: {
-      lat: 51.17906197731903,
-      lng: -1.8265020950682065
-    },
-    creator:'u1'
-  }
-]
+
 
 const UserPlaces = () => {
   const userId = useParams().userId;
