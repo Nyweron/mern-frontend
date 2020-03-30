ee5f682e38176ad3008ec4f0ff711a0f60b885ca
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-03-30 21:09:59 +0200

    Updates

---
+++ precache-manifest.a5c0a7449e34d94e7bb5a800a06d19de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907e21e1d63ed76f15cfbade72b88b56",
+    "url": "/mern/index.html"
+  },
+  {
+    "revision": "3336c42f94c9050ce103",
+    "url": "/mern/static/css/main.fb7c09ee.chunk.css"
+  },
+  {
+    "revision": "017473daf290ade5574f",
+    "url": "/mern/static/js/2.da79f5bd.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/mern/static/js/2.da79f5bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3336c42f94c9050ce103",
+    "url": "/mern/static/js/main.3cbde82b.chunk.js"
+  },
+  {
+    "revision": "9b5c64a4fe5e6f39d968",
+    "url": "/mern/static/js/runtime-main.e1c57ed0.js"
+  }
+]);
