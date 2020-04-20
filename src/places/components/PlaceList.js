1300b13212272fd7bd71f30f99f5fa9f2e05517e
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-20 19:59:04 +0200

    Fixed misspellings

--- src/places/components/PlaceList.js
+++ src/places/components/PlaceList.js
@@ -27,7 +27,7 @@ const PlaceList = props => {
           title={place.title}
           description={place.description}
           address={place.address}
-          creatorId={place.creatorId}
+          creatorId={place.creator}
           coordinates={place.location}
           onDelete={props.onDeletePlace}
         />
