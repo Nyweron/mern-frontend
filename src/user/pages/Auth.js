9b1a447c88ab2adea6a94dd2ec23f75b40e3466f
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-25 17:43:00 +0200

    Upload images from signin

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -38,8 +38,6 @@ const Auth = () => {
   const authSubmitHandler = async (event) => {
     event.preventDefault();
 
-    console.log(formState.inputs);
-
     if (isLoginInMode) {
       try {
         const responseData = await sendRequest(
@@ -57,17 +55,16 @@ const Auth = () => {
       } catch (err) {}
     } else {
       try {
+        const formData = new FormData();
+        formData.append("email", formState.inputs.email.value);
+        formData.append("name", formState.inputs.name.value);
+        formData.append("password", formState.inputs.password.value);
+        formData.append("image", formState.inputs.image.value);
+
         const responseData = await sendRequest(
           "http://localhost:5000/api/users/signup",
           "POST",
-          JSON.stringify({
-            name: formState.inputs.name.value,
-            email: formState.inputs.email.value,
-            password: formState.inputs.password.value,
-          }),
-          {
-            "Content-Type": "application/json",
-          }
+          formData
         );
 
         auth.login(responseData.user.id);
