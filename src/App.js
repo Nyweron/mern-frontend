ba8a7cecd0f4dbd7d563822153b1280aedb0e727
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-04-27 19:08:05 +0200

    Stored the Token in the Browser Storage localStorage

--- src/App.js
+++ src/App.js
@@ -20,6 +20,13 @@ function App() {
 
   const login = useCallback((uid, token) => {
     setToken(token);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+      })
+    );
     setUserId(uid);
   }, []);
 
